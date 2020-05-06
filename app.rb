41f6d79ddacfba54a521fdf3513b77c27cb408d6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-06 12:25:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,19 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+	
+	get '/hello' do
+	  erb :hello
+	end
+	
+	get '/goodbye' do
+	  @name = "Joe"
+	  erb :goodbye
+	end
+	
+	get '/date' do
+	  erb :date
+	end
 
 
 end
